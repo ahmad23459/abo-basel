232ea9bb72e55d1403d296520255bd83c958b819
ahmad23459 <59204861+ahmad23459@users.noreply.github.com>
2020-10-19 13:39:47 +0300

    Add files via upload

---
+++ ahmad.py
@@ -0,0 +1,55 @@
+
+def ahmad ():
+        nem = input("enter name : ")
+        age = input("enter age : ")
+        print ("#(haloo " + nem  +"* ""#old " + age+")")
+mnn = ("\033[0;31mabo basel ")
+nam = ("########################")
+print (nam)
+print ("\033[0;34m* abobasel *")
+print (nam)
+print (" 1:   +  ")
+print (" 2:   ×  ")
+print (" 3:   -  ")
+print (" 4:   ÷  ")
+man = int(input("entar nambar 1.2.3.4 : "))
+son = 0
+if man == 1 :
+	print ("* + *")
+	gmh = int(input(" enter naber 1  : "))
+	mna = int(input(" enter naber 2  : "))
+	son = gmh + mna
+	print ( son)
+elif man == 2 :
+	gmh = int(input(" enter naber 1  : "))
+	mna = int(input(" enter naber 2  : "))
+	print ("* × * ")
+	son = gmh * mna
+	print (son)
+
+elif man == 3 :
+	print (" * - *")
+	gmh = int(input(" enter naber 1  : "))
+	mna = int(input(" enter naber 2  : "))
+	son = gmh - mna
+	print (son)
+elif man == 4 :
+	print ("* ÷ *")
+	gmh = int(input(" enter naber 1  : "))
+	mna = int(input(" enter naber 2  : "))
+	son = gmh / mna
+	print (son)
+
+else :
+	print ("****************<false>*******************")
+
+print (nam)
+print (mnn)
+print (nam)
+
+
+
+
+
+ahmad()
+
